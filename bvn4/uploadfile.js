36cd6ddd9961d26f4ba14ae21a040e7afc9f74ba
cuongmcph27903 <maichicuong2k3@gmail.com>
2023-03-15 10:19:30 +0700

    bvn4

---
+++ bvn4/uploadfile.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.filepath;
+
+      let time = new Date().getTime();
+      console.log(time);
+
+      let filename = files.filetoupload.originalFilename.toString();
+      names = filename.split('.');
+      newFilename = names[0] + time + names[1];
+      console.log(newFilename);
+
+      var newpath = 'C:/Users/maich/Desktop/fileupload/' + newFilename;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
