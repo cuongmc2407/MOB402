28ecfd2eac61ce6e1de1cd8aabf27f4576f39557
cuongmcph27903 <maichicuong2k3@gmail.com>
2023-03-22 02:45:21 +0700

    lab5

--- multer-express/server.js
+++ multer-express/server.js
@@ -1,70 +1,98 @@
-const express = require('express')
-const app = express()
-const port = 8080
-const bodyParser = require('body-parser')
-const multer = require('multer');
+const express = require("express");
+const app = express();
+const port = 8080;
+const bodyParser = require("body-parser");
+const multer = require("multer");
 
-var fs = require('fs');
+var fs = require("fs");
 
-
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(
+  bodyParser.urlencoded({ extended: true })
+);
 
 // SET STORAGE
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+  destination: function (req, file, cb) {
+    dir = "./uploads";
 
-        dir = './uploads';
-
-        if (!fs.existsSync(dir)){
-            fs.mkdirSync(dir, { recursive: true });
-        }
-
-        cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-
-        let fileName = file.originalname;
-
-        let arr = fileName.split('.');
-        if (arr[1] == 'tiff' || arr[1] == 'jfif' || arr[1] == 'bmp' || arr[1] == 'gif' || arr[1] == 'png' || arr[1] == 'jpg' || arr[1] == 'svgz' || arr[1] == 'webp' || arr[1] == 'ico' || arr[1] == 'xbm' || arr[1] == 'pjp' || arr[1] == 'avif') {
-            arr[1] = 'jpeg';
-        }
-        let newFileName = arr[0] + Date.now() + '.' + arr[1];
-
-        console.log(newFileName);
-        cb(null, newFileName)
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
     }
-})
 
-var upload = multer({ storage: storage, limits: {fileSize: 10*1024*1024} })
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    let fileName = file.originalname;
 
-app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
-    const file = req.file
+    let arr = fileName.split(".");
+    if (
+      arr[1] == "tiff" ||
+      arr[1] == "jfif" ||
+      arr[1] == "bmp" ||
+      arr[1] == "gif" ||
+      arr[1] == "png" ||
+      arr[1] == "jpg" ||
+      arr[1] == "svgz" ||
+      arr[1] == "webp" ||
+      arr[1] == "ico" ||
+      arr[1] == "xbm" ||
+      arr[1] == "pjp" ||
+      arr[1] == "avif"
+    ) {
+      arr[1] = "jpeg";
+    }
+    let newFileName =
+      arr[0] + Date.now() + "." + arr[1];
+
+    console.log(newFileName);
+    cb(null, newFileName);
+  },
+});
+
+var upload = multer({
+  storage: storage,
+  limits: { fileSize: 10 * 1024 * 1024 },
+});
+
+app.post(
+  "/uploadfile",
+  upload.single("myFile"),
+  (req, res, next) => {
+    const file = req.file;
     if (!file) {
-        const error = new Error('Please upload a file')
-        error.httpStatusCode = 400
-        return next(error)
+      const error = new Error(
+        "Please upload a file"
+      );
+      error.httpStatusCode = 400;
+      return next(error);
     }
-    res.send(file)
-})
+    res.send(file);
+  }
+);
 
 //Uploading multiple files
-app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
-    const files = req.files
+app.post(
+  "/uploadmultiple",
+  upload.array("myFiles", 12),
+  (req, res, next) => {
+    const files = req.files;
     if (!files) {
-        const error = new Error('Please choose files')
-        error.httpStatusCode = 400
-        return next(error)
+      const error = new Error(
+        "Please choose files"
+      );
+      error.httpStatusCode = 400;
+      return next(error);
     }
-    res.send(files)
-})
+    res.send(files);
+  }
+);
 
-
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/upload.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/upload.html");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(
+    `Example app listening on port ${port}`
+  );
 });
